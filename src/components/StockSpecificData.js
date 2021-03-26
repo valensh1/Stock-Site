ba17c1a7c7c297a447fcaabc3ba9aeee648f16ea
Shaun <valensh1@yahoo.com>
2021-03-26 01:02:09 -0700

    Stock Specific page stats and data updated - no API Key - need to put back in

--- src/components/StockSpecificData.js
+++ src/components/StockSpecificData.js
@@ -12,11 +12,34 @@ const StockSpecificData = props => {
 					<h1>{stockSpecificAPIData[0].companyName}</h1>
 					<h2>{ticker}</h2>
 					<img src={stockSpecificAPIData[0].image}></img>
-					<h4>{stockSpecificAPIData[0].sector}</h4>
-					<h4>{stockSpecificAPIData[0].mktCap}</h4>
-					<h4>${stockSpecificAPIData[0].price}</h4>
-					<h4>${stockSpecificAPIData[0].changes}</h4>
-					<h4>{stockSpecificAPIData[0].range}</h4>
+					<h4>Sector:{stockSpecificAPIData[0].sector}</h4>
+					<h4>Market Cap: {stockSpecificAPIData[1].marketCap}</h4>
+					<h4>Last Price: ${stockSpecificAPIData[1].price}</h4>
+					<h4>Price Change: ${stockSpecificAPIData[1].change}</h4>
+					<h4>% Change: {stockSpecificAPIData[1].changesPercentage}%</h4>
+					<h4>Day High Price: ${stockSpecificAPIData[1].dayHigh}</h4>
+					<h4>Day Low Price: ${stockSpecificAPIData[1].dayLow}</h4>
+					<h4>Volume: {stockSpecificAPIData[1].volume}</h4>
+					<h4>Avg. Volume: {stockSpecificAPIData[1].avgVolume}</h4>
+					<h4>
+						52 Week Range:{' '}
+						{`$${stockSpecificAPIData[1].yearHigh}-$${stockSpecificAPIData[1].yearLow}`}
+					</h4>
+					<h4>
+						50 Day Moving Average: $
+						{stockSpecificAPIData[1].priceAvg50.toFixed(2) * 1}
+					</h4>
+					<h4>
+						200 Day Moving Average: $
+						{stockSpecificAPIData[1].priceAvg200.toFixed(2) * 1}
+					</h4>
+					<h4>PE Ratio: {stockSpecificAPIData[1].pe.toFixed(2) * 1}</h4>
+					<h4>
+						Next Earnings Announcement Date:{' '}
+						{new Date(
+							stockSpecificAPIData[1].earningsAnnouncement
+						).toLocaleDateString()}
+					</h4>
 				</div>
 			) : null}
 		</div>
